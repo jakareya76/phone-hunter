4060af5151c999262dbfc81ebdb35f1e71474178
jakareya76 <jakareya1306@gmail.com>
2024-02-28 13:32:24 +0600

    create modal and fetch mobile data and show it on modal

--- scripts/app.js
+++ scripts/app.js
@@ -42,7 +42,7 @@ const displayPhones = (phones, isShowAll) => {
         <h2 class="text-3xl font-bold">${phone.phone_name}</h2>
         <p class="mb-4">There are many variations of passages of available, but the majority have suffered</p>
         <div class="card-actions justify-center">
-        <button class="btn btn-primary w-full text-xl text-white bg-[#0D6EFD] border-none">Show Details</button>
+        <button onclick="handleShowDetail('${phone.slug}')" class="btn btn-primary w-full text-xl text-white bg-[#0D6EFD] border-none">Show Details</button>
         </div>
     </div>
     `;
@@ -75,6 +75,48 @@ const toggleLoadingSpinner = (isLoading) => {
 
 const handleShowAll = () => {
   handleSearch(true);
+};
+
+const handleShowDetail = async (id) => {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/phone/${id}`
+  );
+
+  const data = await res.json();
+
+  const phone = data.data;
+
+  showPhoneDetails(phone);
+};
+
+const showPhoneDetails = (phone) => {
+  const showDetailContainer = document.getElementById("show-detail-container");
+  showDetailContainer.innerHTML = `
+      <img src="${phone.image}" alt="modal-img" />
+      <h2 class="text-2xl font-bold mt-2">${phone.name}</h2>
+      <div class="text-left space-y-3">
+        <p><span class="font-bold">Storage: </span>${
+          phone?.mainFeatures?.storage
+        }</p>
+        <p><span class="font-bold">GPS: </span>${
+          phone.others?.GPS || "No GPS available"
+        }</p>
+        <p><span class="font-bold">USB: </span>${
+          phone.others?.USB || "No USB available"
+        }</p>
+        <p><span class="font-bold">Bluetooth: </span>${
+          phone.others?.Bluetooth || "No Bluetooth available"
+        }</p>
+        <p><span class="font-bold">WLAN: </span>${
+          phone.others?.WLAN || "No WLAN available"
+        }</p>
+      </div>
+
+
+  `;
+
+  show_modal.showModal();
+  console.log(phone);
 };
 
 loadPhone();
